0eef051aa26caec88c48c34317f7ff9942c98ba0
Simon Foster <funkysi1701@gmail.com>
2015-06-10 22:11:36 +0100

    sort of works as a calculator

--- FSiCalc/FSiCalc/MainPage.xaml.cs
+++ FSiCalc/FSiCalc/MainPage.xaml.cs
@@ -27,6 +27,7 @@ namespace FSiCalc
         {
             this.InitializeComponent();
             SettingsPane.GetForCurrentView().CommandsRequested += onCommandsRequested;
+            Calc.CreateResult();
 
         }
         void onCommandsRequested(SettingsPane settingsPane, SettingsPaneCommandsRequestedEventArgs e)
@@ -41,6 +42,133 @@ namespace FSiCalc
             
 
           
+        }
+
+        public void numberpad(int number)
+        {
+            if (answers.Text == "0") { answers.Text = number.ToString(); }
+            else answers.Text += number;
+        }
+        private void quit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Exit();
+            
+        }
+
+        private void seven_Click(object sender, RoutedEventArgs e)
+        {
+            numberpad(7);
+        }
+
+        private void eight_Click(object sender, RoutedEventArgs e)
+        {
+            numberpad(8);
+        }
+
+        private void nine_Click(object sender, RoutedEventArgs e)
+        {
+            numberpad(9);
+        }
+
+        private void four_Click(object sender, RoutedEventArgs e)
+        {
+            numberpad(4);
+        }
+
+        private void five_Click(object sender, RoutedEventArgs e)
+        {
+            numberpad(5);
+        }
+
+        private void six_Click(object sender, RoutedEventArgs e)
+        {
+            numberpad(6);
+        }
+
+        private void one_Click(object sender, RoutedEventArgs e)
+        {
+            numberpad(1);
+        }
+
+        private void two_Click(object sender, RoutedEventArgs e)
+        {
+            numberpad(2);
+        }
+
+        private void three_Click(object sender, RoutedEventArgs e)
+        {
+            numberpad(3);
+        }
+
+        private void zero_Click(object sender, RoutedEventArgs e)
+        {
+            numberpad(0);
+        }
+
+        private void point_Click(object sender, RoutedEventArgs e)
+        {
+            answers.Text += ".";
+            point.IsEnabled = false;
+        }
+
+        private void signchange_Click(object sender, RoutedEventArgs e)
+        {
+            answers.Text = Calc.ChangeSign(Convert.ToDouble(answers.Text)).ToString();
+        }
+
+        private void equals_Click(object sender, RoutedEventArgs e)
+        {
+            point.IsEnabled = true;
+            Calc.Result.Add(Convert.ToDouble(answers.Text));
+            Calc.Evaluate(Convert.ToDouble(Calc.Result[Calc.Result.Count - 1]), Convert.ToDouble(Calc.Result[Calc.Result.Count - 2]));
+            answers.Text = Calc.ReturnResult();
+        }
+
+        private void plus_Click(object sender, RoutedEventArgs e)
+        {
+            point.IsEnabled = true;
+            Calc.SetOperation("+");
+            Calc.Result.Add(Convert.ToDouble(answers.Text));
+            answers.Text = Calc.ClearResult();
+        }
+
+        private void minus_Click(object sender, RoutedEventArgs e)
+        {
+            point.IsEnabled = true;
+            Calc.SetOperation("-");
+            Calc.Result.Add(Convert.ToDouble(answers.Text));
+            answers.Text = Calc.ClearResult();
+        }
+
+        private void multiply_Click(object sender, RoutedEventArgs e)
+        {
+            point.IsEnabled = true;
+            Calc.SetOperation("*");
+            Calc.Result.Add(Convert.ToDouble(answers.Text));
+            answers.Text = Calc.ClearResult();
+        }
+
+        private void divide_Click(object sender, RoutedEventArgs e)
+        {
+            point.IsEnabled = true;
+            Calc.SetOperation("/");
+            Calc.Result.Add(Convert.ToDouble(answers.Text));
+            answers.Text = Calc.ClearResult();
+        }
+
+        private void clear_Click(object sender, RoutedEventArgs e)
+        {
+            point.IsEnabled = true;
+            Calc.Result.Add(Convert.ToDouble(answers.Text));
+            answers.Text = Calc.ClearResult();
+        }
+
+        private void sqrt_Click(object sender, RoutedEventArgs e)
+        {
+            point.IsEnabled = true;
+            //Calc.SquareRoot(answers.Text)
+            Calc.Result.Add(Convert.ToDouble(answers.Text));
+            answers.Text = Calc.ClearResult();
         }
     }
 }
