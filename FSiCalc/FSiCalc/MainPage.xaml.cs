ab42bcb1d47548e7bdeacdfde8a2f5c1a56ff5bc
Simon Foster <funkysi1701@gmail.com>
2015-06-10 20:28:41 +0100

    Initial commit of my windows app only shows about info so far

---
+++ FSiCalc/FSiCalc/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.ApplicationSettings;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace FSiCalc
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            SettingsPane.GetForCurrentView().CommandsRequested += onCommandsRequested;
+
+        }
+        void onCommandsRequested(SettingsPane settingsPane, SettingsPaneCommandsRequestedEventArgs e)
+        {
+            SettingsCommand generalCommand = new SettingsCommand("about", "About",
+                (handler) =>
+                {
+                    SettingsFlyout1 sf = new SettingsFlyout1();
+                    sf.Show();
+                });
+            e.Request.ApplicationCommands.Add(generalCommand);
+            
+
+          
+        }
+    }
+}
