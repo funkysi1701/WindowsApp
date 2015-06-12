8fb20d1e28547d43dab426b9085def68efcd093f
Simon Foster <funkysi1701@gmail.com>
2015-06-12 16:10:17 +0100

    Created Import script and base table

---
+++ StarTrek/StarTrekData/dbo/Stored Procedures/ImportXML.sql
@@ -0,0 +1,32 @@
+﻿
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[ImportXML]
+@Table bit
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+if(@table = 1)
+BEGIN
+CREATE TABLE XMLwithOpenXML
+(
+Id INT IDENTITY PRIMARY KEY,
+XMLData XML,
+LoadedDateTime DATETIME
+)
+END
+
+INSERT INTO XMLwithOpenXML(XMLData, LoadedDateTime)
+SELECT CONVERT(XML, BulkColumn) AS BulkColumn, GETDATE() 
+FROM OPENROWSET(BULK 'C:\Projects\WindowsApp\StarTrek\enmemoryalpha_pages_current.xml', SINGLE_BLOB) AS x;
+
+
+SELECT * FROM XMLwithOpenXML
+END
+
---
+++ StarTrek/StarTrekData/dbo/Tables/XMLwithOpenXML.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[XMLwithOpenXML] (
+    [Id]             INT      IDENTITY (1, 1) NOT NULL,
+    [XMLData]        XML      NULL,
+    [LoadedDateTime] DATETIME NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
